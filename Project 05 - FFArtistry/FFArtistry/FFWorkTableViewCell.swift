fabb12839b8752c36066f03b363e362396b7d0be
18698807852@163.com <18698807852@163.com>
2019-06-27 11:40:47 +0800

    FFArtistry

---
+++ Project 05 - FFArtistry/FFArtistry/FFWorkTableViewCell.swift
@@ -0,0 +1,17 @@
+    //
+//  FFWorkTableViewCell.swift
+//  FFArtistry
+//
+//  Created by zhou on 2019/6/26.
+//  Copyright © 2019 MissZhou. All rights reserved.
+//
+
+import UIKit
+
+class FFWorkTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var workImageView: UIImageView!
+    @IBOutlet weak var workTitleLabel: UILabel!
+    @IBOutlet weak var workTextView: UITextView!
+    
+}
---
+++ Project 05 - FFArtistry/FFArtistry/Work.swift
@@ -0,0 +1,16 @@
+//
+//  Work.swift
+//  FFArtistry
+//
+//  Created by zhou on 2019/6/26.
+//  Copyright © 2019 MissZhou. All rights reserved.
+//
+
+import UIKit
+
+struct Work {
+    let title: String
+    let image: UIImage
+    let info: String
+    var isExpanded: Bool
+}
